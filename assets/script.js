393177d71e067d50b593d8a0f5b8a2d5e8c64385
theon87 <theonfrancis17@gmail.com>
2024-03-24 15:35:17 -0400

    added code to JavaSript file

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,39 @@
+//This is an array that stores all the elements
 const topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 
-for (let x = 0; x < topics.length; x++) {
-    console.log(topics[x])
+/*This randomTopic variable allows the selectTopic() function to work without having to 
+    enter each index manually - like topic[0]
+*/
+const randomTopic = topics[Math.floor(Math.random() * topics.length)];
+
+/*This is a function wrapped around a for loop
+    The for loop selects each index in the array until all indicies are referenced
+*/
+function listTopics() {
+    for (let x = 0; x < topics.length; x++) {
+        console.log(topics[x])
+    }    
 }
 
-/*
-if (topic === 'HTML') {
-  console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
-  console.log("Let's study CSS!");
-} else if (topic === 'Git') {
-  console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
-  console.log("Let's study JavaScript!");
-} else {
-  console.log('Please try again!');
+//This is a function wrapped around an if...else if conditional
+function selectTopic() {
+    if (randomTopic === 'HTML') {
+      console.log("Let's study HTML!");
+    } else if (randomTopic === 'CSS') {
+      console.log("Let's study CSS!");
+    } else if (randomTopic === 'Git') {
+      console.log("Let's study Git!");
+    } else if (randomTopic === 'JavaScript') {
+      console.log("Let's study JavaScript!");
+    } else {
+      console.log('Please try again!');
+    }
 }
-*/
+
+console.log('Here are the topics we learned through Prework:');
+
+//A function will not execute unless it is called as it is in this line
+listTopics();
+
+console.log('Which topic should we study first?');
+selectTopic();
